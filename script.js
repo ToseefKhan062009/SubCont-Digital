2e2ec8ebba2d610b776d084e860a6d5cd6d0ffc4
ToseefKhan062009 <ToseefKhan062009@gmail.com>
2025-09-28 15:47:51 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for internal anchors
-document.addEventListener('click', (e)=>{
-  const a = e.target.closest('a[href^="#"]');
-  if(a){
-    const id = a.getAttribute('href').slice(1);
-    const el = document.getElementById(id);
-    if(el){ e.preventDefault(); el.scrollIntoView({behavior:'smooth'}); }
-  }
-});
